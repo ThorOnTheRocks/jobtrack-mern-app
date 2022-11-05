28e10b1b0e9f98e3c7fa7c24c087dbdc218c5d8c
ThorOnTheRocks <gianluca.galota@gmail.com>
2022-11-05 17:12:30 +0000

    setup client app

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import Landing from './pages/Landing';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Landing.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/Testing';
+import { Logo } from '../components';
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby slow-carb vibecession hot chicken quinoa direct trade
+            master cleanse man bun scenester yes plz stumptown humblebrag fanny
+            pack jianbing. Synth literally edison bulb, la croix vibecession
+            blue bottle taiyaki green juice man braid tumblr pour-over tbh prism
+            try-hard.
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
