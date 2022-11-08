44dad23a568733773e24e92b6901810d1ddf4a46
ThorOnTheRocks <gianluca.galota@gmail.com>
2022-11-08 22:53:48 +0000

    errorHandler middleware created

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
+  throw new Error('error');
   res.send('Here I am');
 })
 
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 const port = process.env.PORT || 5000;
